b7a71bf05471bde110e1214003392c32187c077f
amirtbi <torabi46@gmail.com>
2023-01-02 17:22:21 +0330

    upload folders

---
+++ src/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>default layout</h1>
+
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,15 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
+import Layouts from "vite-plugin-vue-layouts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Pages({
-      router: {
-        extendRoutes(routes, resolve) {
-          routes.push({
-            name: "custom",
-            path: "*",
-            component: resolve(__dirname, "pages/404.vue"),
-          });
-        },
-      },
       exclude: ["**/components/*.vue", "src/pages/**/components/*.vue"],
       dirs: [{ dir: resolve(__dirname, "./src/pages"), baseRoute: "" }],
     }),
+    Layouts(),
   ],
 });
