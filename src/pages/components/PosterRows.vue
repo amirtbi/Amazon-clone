8f71beb20a04bbee41a9cce82243725a52aef02e
Amir <torabi46@gmail.com>
2023-01-23 02:01:41 +0330

    define and and poster rows for temporary purposes

---
+++ src/pages/components/PosterRows.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  movieList: [];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <VCol :col="3">
+    <!--Title -->
+    <h1>{{ title }}</h1>
+    <!--Poster-->
+    <v-img
+      v-for="(movie, index) in movieList"
+      :key="index"
+      :src="movie.bakcground"
+    ></v-img>
+  </VCol>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-import { reactive, ref, onMounted, nextTick } from "vue";
+import { reactive, ref, onMounted } from "vue";
 import HomeScreenLayout from "@/layouts/homeScreen.vue";
 import BannerContent from "@/components/layouts/BannerContent.vue";
 
 import { useMovie } from "@/composable/usefetchhMovie";
-
+import type { genre } from "@/models/movieGenres";
+const genres: genre[] = ["comedy", "documentary", "horror", "romantic"];
 const movieInfo = reactive<{
   title: string;
   description: string;
   background: string;
 }>({ title: "", description: "", background: "" });
 
+const movies = ref<{ genre: string; list: any[] }[]>([]);
 const { fetchMovieGenre, fetchOriginalMovie } = useMovie();
 
 onMounted(async () => {
   const { title, description, background } = await fetchOriginalMovie();
+
+  // Requests for filling movie genre rows
 
   /**
    * Set movieInfo
@@ -23,9 +27,13 @@ onMounted(async () => {
   movieInfo.description = description;
   movieInfo.background = background;
 
-  await nextTick();
-  await fetchMovieGenre("comedy");
-  // await fetchData();
+  for (const genre of genres) {
+    const response = await fetchMovieGenre(genre);
+
+    movies.value.push({ genre: genre, list: await response });
+  }
+
+  console.log("Response for comedy", await movies.value);
 });
 </script>
 
@@ -45,6 +53,22 @@ onMounted(async () => {
         :description="movieInfo.description"
       />
     </template>
+    <template #poster-rows>
+      <VRow v-for="(movie, index) in movies" :key="index" class="row-col">
+        <VCol cols="12">
+          <h1 class="text-white pa-2">{{ movie.genre }}</h1>
+        </VCol>
+        <VCol class="d-flex" cols="12">
+          <v-img
+            class="poster-img mx-2"
+            v-for="(item, k) in movie.list"
+            :key="k"
+            :src="item.background"
+          ></v-img>
+        </VCol>
+      </VRow>
+    </template>
+
     <template #main> </template>
   </HomeScreenLayout>
 </template>
@@ -54,5 +78,14 @@ onMounted(async () => {
   position: absolute;
   z-index: -1;
   width: 100%;
+}
+.poster-img {
+  max-height: 150px;
+}
+.row-col {
+  max-width: 100%;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  padding: 20px;
 }
 </style>
