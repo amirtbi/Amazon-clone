1ff9caf1096de3415df8033d26a846dfb342fe31
Amir <torabi46@gmail.com>
2023-01-27 23:56:01 +0330

    Define more types for IMovieItem interface

--- src/models/movieGenres.ts
+++ src/models/movieGenres.ts
@@ -1,6 +1,26 @@
-export type genre = "comedy" | "horror" | "romantic" | "documentary";
+export type genre = "comedy" | "horror" | "romantic" | "documentary" | "Netflix origin";
+
+
+
 
 export interface MovieGenreRows {
     title:string;
     posterPath:string
 }
+
+export interface IMovieItem {
+    title:string;
+    background:string;
+    description:string;
+    vote:number
+    releaseDate:string
+
+}
+
+export type MovieCover =  Pick<IMovieItem,"title" | "background" |"description">
+
+export interface IMoviesByGenre{
+    genre:genre;
+    list:IMovieItem[]
+}
+
